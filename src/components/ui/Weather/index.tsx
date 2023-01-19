98a2aa87ad2be3e1fef88a431ac1b1de22041567
cristian.restituyo@friday.de <cristian.restituyo@friday.de>
2023-01-19 22:06:28 +0100

    test: add weather and utils test

--- src/components/ui/Weather/index.tsx
+++ src/components/ui/Weather/index.tsx
@@ -14,7 +14,7 @@ const Weather: React.FC<Props> = ({ data }) => {
   }
 
   return (
-    <div className={styles.weather}>
+    <div className={styles.weather} data-testid="weather">
       <div className={styles.mainWrapper}>
         <div className={styles.mainLeftColumn}>
           <Image
@@ -22,17 +22,18 @@ const Weather: React.FC<Props> = ({ data }) => {
             width="100"
             height="100"
             alt="Weather Icon"
+            data-testid="weather-image"
           />
           <div className={styles.minMaxMainTempWrapper}>
             <p>{data?.weather[0].main}</p>
             <div>
-              <span> H: {formatTemperature(data?.main.temp_max)}</span>
-              <span> L: {formatTemperature(data?.main.temp_min)}</span>
+              <span data-testid="high-temp-text">H: {formatTemperature(data?.main.temp_max)}</span>
+              <span data-testid="low-temp-text">L: {formatTemperature(data?.main.temp_min)}</span>
             </div>
           </div>
         </div>
         <div className={styles.mainRightColumn}>
-          <p className={styles.tempText}>
+          <p className={styles.tempText} data-testid="temp-text">
             {formatTemperature(data?.main.temp)}
           </p>
         </div>
